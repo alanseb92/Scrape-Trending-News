7f2becb5c2ecd52c158e5b79edaea9a06958ddad
Alan Sebastián García <104204055+alanseb92@users.noreply.github.com>
2022-09-29 18:33:59 -0300

    Update scrape_news.py

--- scrape_news.py
+++ scrape_news.py
@@ -1,12 +1,16 @@
+# as the returning output will be in the form of a dictionary so I will convert it into a pandas DataFrame
+
 from GoogleNews import GoogleNews
 news = GoogleNews(period='1d')
-news.search("India")
+news.search("Argentina")
 result = news.result()
 import pandas as pd
 data = pd.DataFrame.from_dict(result)
 data = data.drop(columns=["img"])
 data.head() 
 
+
+#click on the links to read the complete news
 for i in result:
     print("Title : ", i["title"])
     print("News : ", i["desc"])
